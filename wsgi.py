c4301b4ea149b0683b6046b9b12837186a660527
chatbot2021 <37211719+chatbot2021@users.noreply.github.com>
2018-03-09 10:44:33 +0000

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import os
+
+
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
+
+from app import app as application
