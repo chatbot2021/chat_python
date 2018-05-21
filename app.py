9a6e74d0871b780d41ecf23ed1482c308c67ddc6
chatbot2021 <37211719+chatbot2021@users.noreply.github.com>
2018-05-21 05:43:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1817,6 +1817,7 @@ def chatapiai():
                 print "no"+from_number
                 cur.execute("update chat_user set context=%s where mobile_no=%s"%(fetcontext1,from_number))
 		print dataDict["output"]
+		print sys.getdefaultencoding()
 		print dataDict["output"]["text"][0]
                 out_msg= dataDict["output"]["text"][0]		
                 action=dataDict["output"]["action"]
